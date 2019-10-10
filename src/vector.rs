d1c8f88ca9db6923bba30984251bf7dd4f76a7d7
Aron Bergman <bathingrad@gmail.com>
2019-10-10 23:00:23 +0200

    Implement rotations for Vector3

--- src/vector.rs
+++ src/vector.rs
@@ -47,6 +47,47 @@ impl Vector3 {
 			self.x * v.y - self.y * v.x
 		)
 	}
+
+	pub fn rotate_around_axis(&mut self, axis: Vector3, angle: f32) {
+		let len = axis.length();
+		let mut unit = axis.clone();
+
+		if len != 1.0 {
+			unit.scale(1.0 / len);
+		}
+
+		let old_x = self.x;
+		let old_y = self.y;
+		let old_z = self.z;
+
+		// rotate the vector around the unit vector `axis` as described on
+		// en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
+
+		self.x =
+			old_x * (angle.cos() + axis.x * axis.x * (1.0 - angle.cos())) +
+			old_y * (axis.x * axis.y * (1.0 - angle.cos()) - axis.z * angle.sin()) +
+			old_z * (axis.x * axis.z * (1.0 - angle.cos()) + axis.y * angle.sin());
+		self.y =
+			old_x * (axis.y * axis.x * (1.0 - angle.cos()) + axis.z * angle.sin()) +
+			old_y * (angle.cos() + axis.y * axis.y * (1.0 - angle.cos())) +
+			old_z * (axis.y * axis.z * (1.0 - angle.cos()) - axis.x * angle.sin());
+		self.z =
+			old_x * (axis.z * axis.x * (1.0 - angle.cos()) - axis.y * angle.sin()) +
+			old_y * (axis.z * axis.y * (1.0 - angle.cos()) + axis.x * angle.sin()) +
+			old_z * (angle.cos() + axis.z * axis.z * (1.0 - angle.cos()));
+	}
+
+	pub fn rotate_around_x(&mut self, angle: f32) {
+		self.rotate_around_axis(Vector3::new(1.0, 0.0, 0.0), angle);
+	}
+
+	pub fn rotate_around_y(&mut self, angle: f32) {
+		self.rotate_around_axis(Vector3::new(0.0, 1.0, 0.0), angle);
+	}
+
+	pub fn rotate_around_z(&mut self, angle: f32) {
+		self.rotate_around_axis(Vector3::new(0.0, 0.0, 1.0), angle);
+	}
 }
 
 #[derive(Clone)]
