3e893e898b18e933dac6850cc846bcd1e45839fd
Aron Bergman <bathingrad@gmail.com>
2019-10-11 15:04:41 +0200

    Create and render a simple scene

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,40 @@ mod object;
 mod camera;
 mod scene;
 
-fn main() {
+use vector::Vector3;
+use point::Point3;
+use camera::Camera;
+use scene::Scene;
+use object::Object;
+use object::Sphere;
+use object::Plane;
+use color::Rgb;
 
+fn main() {
+	let red = Rgb::new(255, 0, 0);
+	let green = Rgb::new(0, 255, 0);
+	let blue = Rgb::new(0, 0, 255);
+
+	let camera = Camera::new(
+		Point3::new(0.0, 0.0, 0.0),
+		std::f32::consts::PI / 2.0,
+		Vector3::new(1.0, 0.0, 0.0)
+	);
+
+	let sphere = Sphere::new(Point3::new(4.0, 0.0, -1.0), 2.0, blue);
+	let sphere2 = Sphere::new(Point3::new(4.0, 3.0, 0.0), 1.0, red);
+	let plane = Plane::new(
+		Point3::new(0.0, 0.0, -1.0),
+		Vector3::new(0.0, 0.0, -1.0),
+		green
+	);
+
+	let mut scene = Scene::new(camera);
+
+	scene.add_object(Object::Sphere(sphere));
+	scene.add_object(Object::Sphere(sphere2));
+	scene.add_object(Object::Plane(plane));
+
+	let image = scene.render();
+	image.save("render.png").unwrap();
 }
