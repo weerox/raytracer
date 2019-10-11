855244dbaecbe9b4efd8b1499657d0fcf94c0842
Aron Bergman <bathingrad@gmail.com>
2019-10-11 13:57:03 +0200

    Add Plane

--- src/object.rs
+++ src/object.rs
@@ -1,9 +1,11 @@
 use crate::point::Point3;
+use crate::vector::Vector3;
 use crate::color::Rgb;
 
 #[derive(Copy, Clone)]
 pub enum Object {
 	Sphere(Sphere),
+	Plane(Plane),
 }
 
 #[derive(Copy, Clone)]
@@ -18,6 +20,29 @@ impl Sphere {
 		Sphere {
 			center: center,
 			radius: radius,
+			color: color,
+		}
+	}
+}
+
+#[derive(Copy, Clone)]
+pub struct Plane {
+	position: Point3,
+	normal: Vector3,
+	color: Rgb,
+}
+
+impl Plane {
+	pub fn new(position: Point3, normal: Vector3, color: Rgb) -> Plane {
+		let mut normal = normal;
+		// make sure that the normal is a unit vector
+		if normal.length() != 1.0 {
+			normal.scale(1.0 / normal.length());
+		}
+
+		Plane {
+			position: position,
+			normal: normal,
 			color: color,
 		}
 	}
